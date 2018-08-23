2f5f531a556021fc8a59e0e1fd7445a7f0fd94b5
李吉 <q896126514@gamil.com>
2018-08-23 22:00:00 +0800

    Initial commit

---
+++ apps/goods/adminx.py
@@ -0,0 +1,60 @@
+import xadmin
+from .models import Goods, GoodsCategory, GoodsImage, GoodsCategoryBrand, Banner
+
+
+class GoodsAdmin(object):
+    # 显示的列
+    list_display = ["name", "click_num", "sold_num", "fav_num", "goods_num", "market_price",
+                    "shop_price", "goods_brief", "goods_desc", "is_new", "is_hot", "add_time"]
+    # 搜索关键词
+    search_fields = ['name', ]
+    #列表页可以直接编辑
+    list_editable = ["is_hot", ]
+    #过滤器
+    list_filter = ["name", "click_num", "sold_num", "fav_num", "goods_num", "market_price",
+                   "shop_price", "is_new", "is_hot", "add_time", "category__name"]
+    style_fields = {"goods_desc": "ueditor"}
+
+    class GoodsImagesInline(object):
+        model = GoodsImage
+        exclude = ["add_time"]
+        extra = 1
+        style = 'tab'
+
+    inlines = [GoodsImagesInline]
+
+
+class GoodsCategoryAdmin(object):
+    list_display = ["name", "category_type", "parent_category", "add_time"]
+    list_filter = ["category_type", "parent_category", "name"]
+    search_fields = ['name', ]
+
+
+class GoodsBrandAdmin(object):
+    #list_display = ["category", "image", "name", "desc"]
+    list_display = ["image", "name", "desc"]
+
+    def get_context(self):
+        context = super(GoodsBrandAdmin, self).get_context()
+        if 'form' in context:
+            context['form'].fields['category'].queryset = GoodsCategory.objects.filter(category_type=1)
+        return context
+
+
+class BannerGoodsAdmin(object):
+    list_display = ["goods", "image", "index"]
+
+
+class HotSearchAdmin(object):
+    list_display = ["keywords", "index", "add_time"]
+
+
+class IndexAdAdmin(object):
+    list_display = ["category", "goods"]
+
+
+xadmin.site.register(Goods, GoodsAdmin)
+xadmin.site.register(GoodsCategory, GoodsCategoryAdmin)
+xadmin.site.register(Banner, BannerGoodsAdmin)
+xadmin.site.register(GoodsCategoryBrand, GoodsBrandAdmin)
+
